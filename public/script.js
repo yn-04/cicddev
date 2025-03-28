3b1a67b09d693f1ec79a1b5438a92c10a85a1a61
yn-04 <methawadee666@gmail.com>
2025-03-28 23:58:24 +0700

    Delete public/script.js

--- public/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-function generateQuestion() {
-  fetch('/.netlify/functions/script')  // เรียกใช้งานฟังก์ชันจาก Netlify
-    .then(response => response.json())  // แปลงข้อมูลเป็น JSON
-    .then(data => {
-      document.getElementById('question-box').innerHTML = `<p>${data.question}</p>`;
-    })
-    .catch(error => console.error('Error:', error));  // แสดงข้อผิดพลาดหากเกิดขึ้น
-}
